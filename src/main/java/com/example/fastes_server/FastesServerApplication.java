32fd2392871637209b19e3ce16cd03884e86d8fc
Ratchet34702 <79446167+Ratchet34702@users.noreply.github.com>
2022-04-21 16:00:11 +0300

    Adding base files

---
+++ src/main/java/com/example/fastes_server/FastesServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastes_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastesServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,22 @@
+/*
+package model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ft_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_user")
+    private int id;
+    @Column(name = "user_name ")
+    private String userName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "user_password")
+    private String password;
+}
+*/
+
