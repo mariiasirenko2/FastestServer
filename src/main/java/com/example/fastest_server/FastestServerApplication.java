0948800a3cdcc8fe4cbfc77ea664817d624583e6
Mariia <mariiasirenko2@gmail.com>
2022-04-30 18:01:48 +0300

    Add entities and connections

--- src/main/java/com/example/fastest_server/FastestServerApplication.java
+++ src/main/java/com/example/fastest_server/FastestServerApplication.java
@@ -1,13 +1,13 @@
-package com.example.fastes_server;
+package com.example.fastest_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FastesServerApplication {
+public class FastestServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FastesServerApplication.class, args);
+		SpringApplication.run(FastestServerApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/fastest_server/FastestServerApplicationTests.java
+++ src/test/java/com/example/fastest_server/FastestServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.fastes_server;
+package com.example.fastest_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FastesServerApplicationTests {
+class FastestServerApplicationTests {
 
 	@Test
 	void contextLoads() {
