96111c0912733a13b13970c33997e7071e3a119d
Mariia Sirenko <mariiasirenko2@gmail.com>
2022-12-13 20:48:38 +0200

    test updates to run opencv code

--- src/main/java/com/example/fastest_server/FastestServerApplication.java
+++ src/main/java/com/example/fastest_server/FastestServerApplication.java
@@ -1,14 +1,22 @@
 package com.example.fastest_server;
 
+import nu.pattern.OpenCV;
+import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FastestServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FastestServerApplication.class, args);
 
-	}
+  static {
+    nu.pattern.OpenCV.loadLocally();
+   // System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(FastestServerApplication.class, args);
+
+  }
 
 }
